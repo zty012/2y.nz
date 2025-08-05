dc187c7ae8d734c2906aa0b0b858b50b3766e35f
zty012 <me@zty012.de>
2025-08-05 18:04:28 +0800

    ✨ Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://2y.nz/",
 	base: "/",
 	trailingSlash: "ignore",
 	integrations: [
