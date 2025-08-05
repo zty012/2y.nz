83a02bf18c18804cf4e7fafe7b8e1e8324cbe94d
zty012 <me@zty012.de>
2025-08-05 18:13:32 +0800

    ✨ Update

--- src/config.ts
+++ src/config.ts
@@ -17,12 +17,12 @@ export const siteConfig: SiteConfig = {
 	},
 	banner: {
 		enable: true,
-		src: "https://eo-pic.2x.nz/h", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		src: "/img/banner.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: true, // Display the credit text of the banner image
+			text: "空色天絵 / NEO TOKYO NOIR 01", // Credit text to be displayed
+			url: "https://www.pixiv.net/artworks/111024784", // (Optional) URL link to the original artwork or artist's page
 		},
 	},
 	toc: {
